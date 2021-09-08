d9f76ef8e5e7291aba16a2248c7d72166e24f8b4
Vishwanath <vishwanathvishwabai@gmail.com>
2021-09-08 20:18:30 +0530

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
 const hamburger = document.querySelector('.hamburger');
+const showBtn = document.getElementById('show');
+const showMoreIcon = document.getElementById('showmore__icon');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('open');
+})
+
+
+
+
+showBtn.addEventListener('click', () => {
+    
+    if (showMoreIcon.classList.toggle('active')) {
+        showBtn.innerHTML = `
+        <i class="fas fa-caret-up"></i>
+        <p>Show Less</p>
+    `;
+    }else{
+        showBtn.innerHTML = `
+        <i class="fas fa-caret-down"></i>
+        <p>Show More</p>
+    `;
+    }
+    
 })
